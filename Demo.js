a7f668e83005b56ade0e9659d6c4d094ea4d3054
utkarsh <utkarshbadoni2000@gmail.com>
2023-02-04 02:46:29 +0530

    query

--- Demo.js
+++ Demo.js
@@ -1,7 +1,24 @@
-var item =document.getElementsByClassName("list-group-item");
-console.log(item);
-item[2].style.backgroundColor='green';
-item[0].style.fontWeight= 'bold';
-// TagName
-var tag=document.getElementsByTagName("li");
-tag[0].style.backgroundColor="pink";
+// var item =document.getElementsByClassName("list-group-item");
+// console.log(item);
+// item[2].style.backgroundColor='green';
+// item[0].style.fontWeight= 'bold';
+
+// // TagName
+// var tag=document.getElementsByTagName("li");
+// tag[0].style.backgroundColor="pink";
+
+// query selector
+var querry=document.querySelector(".list-group-item");
+querry[2].style.backgroundColor= "green";
+querry.textContext= " ";
+
+// //querySelctorAll
+// var odd=document.querySelectorAll("li:nth-child(odd)");
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor=" grey";
+// }
+
+// var even=document.querySelectorAll("li:nth-child(even)");
+// for(var i=0;i<odd.length;i++){
+//     even[i].style.backgroundColor=" lightblue";
+// }
