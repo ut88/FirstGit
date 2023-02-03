bcafde7c676cfcd5c9dc7cb48bcfc8b98c0e4e17
utkarsh <utkarshbadoni2000@gmail.com>
2023-02-04 01:41:43 +0530

    tagname included

--- Demo.js
+++ Demo.js
@@ -2,3 +2,6 @@ var item =document.getElementsByClassName("list-group-item");
 console.log(item);
 item[2].style.backgroundColor='green';
 item[0].style.fontWeight= 'bold';
+// TagName
+var tag=document.getElementsByTagName("li");
+tag[0].style.backgroundColor="pink";
